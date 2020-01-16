c0bafdc35b0981d78036bc5c27967229fe1d4988
keiralien <jen.murphy8200@gmail.com>
2020-01-15 18:11:17 -0600

    Class 3 Studio

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,21 @@
+package restaurant;
+
+import java.util.Date;
+
+public class Menu {
+
+    // Variables
+    private Date lastUpdated = new Date();
+
+    // Getters & Setters
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    // Methods
+
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,46 @@
+package restaurant;
+
+public class MenuItem {
+
+    // Variables
+    private Double price;
+    private String description;
+    private String category;
+    private Boolean newItem = false;
+
+    // Getters & Setters
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getNewItem() {
+        return newItem;
+    }
+
+    public void setNewItem(Boolean newItem) {
+        this.newItem = newItem;
+    }
+
+    // Methods
+
+}
